b8a5626a39ed2d9afcae343610442a9355f4d450
David Johnson <43194595+narly-david@users.noreply.github.com>
2022-10-14 21:04:01 +0000

    update imports

--- app.py
+++ app.py
@@ -1,9 +1,4 @@
-import pandas
-import pandas_profiling
-import streamlit as st
-
 import pandas as pd
-import pandas_profiling
 import streamlit as st
 
 from streamlit_pandas_profiling import st_profile_report
